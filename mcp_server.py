bb493f9675ba372891436890785effed66e596c5
PhoebeBaek <96410002+PhoebeBaek@users.noreply.github.com>
2025-05-11 22:33:13 +0900

    Update mcp_server.py

--- mcp_server.py
+++ mcp_server.py
@@ -1,7 +1,9 @@
+import os
 from mcp.server.fastmcp import FastMCP
 import config
 import asyncio
 import io
+import re
 import base64
 import vertexai
 from vertexai.vision_models import Image, MultiModalEmbeddingModel
@@ -17,15 +19,15 @@ from pymongo import MongoClient
 mcp = FastMCP("mcp_server")
 
 @mcp.tool()
-def input_image(image_path: str) -> str:
+def input_image(image_path):
     """
     Receiving multimodal menu image and analyze the menu.
     
     Args:
-        image_path: Path to the image file containing a menu
+        image_path: Path to the image file containing a menu.
         
     Returns:
-        Analysis about the menu.
+        Ingredients analysis about the menu.
     """
     # Open the uploaded image
     with open(image_path, "rb") as image_file:
@@ -43,7 +45,20 @@ def input_image(image_path: str) -> str:
     message = HumanMessage(
         content=[
             {"type": "text", 
-            "text": "You are a menu analyzer. Your task is to analyze the provided menu image and return a description about the menu."},
+            "text": """
+                    You are a menu analyzer. Your are responsible for the tasks based on the rules below.
+                    <Tasks>
+                    1. Analyze menu images
+                    2. Analyze what ingredients are required to cook the menu in Korean.
+                    </Tasks>
+
+                    <Rules>
+                    1. Return ONLY a Python list of ingredients, nothing else
+                    2. Each element in array should be a string
+                    3. Do not include any other text or formatting
+                    </Rules>
+                    """
+            },
             {
                 "type": "image",
                 "source": {
@@ -61,91 +76,57 @@ def input_image(image_path: str) -> str:
 
 
 @mcp.tool()
-def mongodb_query(input_query: str):
+def mongodb_query(input_items):
     """
     Implement text to sql and run query in MongoDB.
     Args:
-        input_query: natural language that needs to be converted into MongoDB query api.
+        input_items: String representation of a list of items from `menu_analysis_assistant`.
         
     Returns:
         Query output return by MongoDB.
     """
 
     client = MongoClient(config.URI)
-    db = client["dining-ai"]
+    db = client["dining_ai"]
     collection = db["items"]
 
     # Initialize the model client
     llm = ChatBedrock(
         region="us-east-1",
-        # provider = "anthropic",
-        # model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
-        model_id = "amazon.nova-pro-v1:0"
+        provider = "anthropic",
+        model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
+        # model_id = "amazon.nova-pro-v1:0"
     )
     #Define a message
     message = HumanMessage(
         content=[
             {
                 "type": "text",
-                "text": f"""
-                    You are a MongoDB expert. Your task is converting the input query to MongoDB pymongo API following the rules.
-                    Below is the schema sample in sample_mflix.movies namespace in MongoDB for your reference.
-                    
-                    <Input_query>
-                    {input_query}
-                    </Input_query>
+                    "text": f"""
+                    You are a MongoDB expert. Your task is writing MongoDB aggregation API to query a item based on the input_items.
+                    The input_items is {input_items}.
+                    Below is the schema sample in dining_ai.items namespace in MongoDB for your information.
 
                     <Rules>
-                    1. Analzy the input thoroughly in order to understand what data needs to be found.
-                    2. Return the query API method and filter only and do not add any explanation.
-                    3. Do not add any extra filter on query.
-                    4. Return the filter in valid JSON format.
-                    5. Always exclude 'embedding' field in the query result.
-                    6. Do not include any markdown formatting (like ```json) in your response.
+                        1. Find the first item in input_items to understand what data needs to be found. If the input_items is ['사과', '부추'], only '사과' is used to query the item.
+                        2. Create a MongoDB aggregation API for only the first item based on the input.
+                        3. Add limit stage to limit the number of results to 3.
+                        4. Return MongoDB aggregation API in valid JSON format.
+                        5. Do not include any markdown formatting and additional filters.
                     </Rules>
-
                     <Example>
-                    Input: Search for one movie which released in 2000 and with the rating higher than 4.
-                    Return: find({{"year": 2000, "imdb.rating": {{"$gt": 4}}}}, {{"embedding": 0}}).limit(1)
+                    Input: {{
+                                "source":"menu_analysis_assistant"
+                                "models_usage":NULL
+                                "metadata":{{}}
+                                "content":"[TextContent(type='text', text="['오징어', '부추']", annotations=None)]"
+                                "type":"ToolCallSummaryMessage"
+                            }}
+                    Return: [{{'$search':{{'text':{{'query':'고기','path':'title'}}}}}},{{'$limit':3}}]
                     </Example>
 
                     <Schema sample>
-                    {{
-                    "title": "The Arrival of a Train",
-                    "year": {{
-                        "$numberInt": "1896"
-                    }},
-                    "lastupdated": "2015-08-15 00:02:53.443000000",
-                    "type": "movie",
-                    "directors": [
-                        "Auguste Lumière",
-                        "Louis Lumière"
-                    ],
-                    "imdb": {{
-                        "rating": {{
-                        "$numberDouble": "7.3"
-                        }},
-                        "votes": {{
-                        "$numberInt": "5043"
-                        }},
-                        "id": {{
-                        "$numberInt": "12"
-                        }}
-                    }},
-                    "cast": [
-                        "Madeleine Koehler"
-                    ],
-                    "countries": [
-                        "France"
-                    ],
-                    "genres": [
-                        "Documentary",
-                        "Short"
-                    ],
-                    "num_mflix_comments": {{
-                        "$numberInt": "1"
-                        }}
-                    }}
+                    {{"_id":{{"$oid":"681715a9cc50fd56598796f6"}},"title":"미국산 프라임 척아이롤(목심+등심) 100G/소고기","lprice":"16380","hprice":"","mallName":"Homeplus","productId":"82539599247","productType":"2","brand":"","maker":"","category1":"식품","category2":"축산물","category3":"쇠고기","category4":"수입산쇠고기"}}
                     </Schema sample>
                 """
             }
@@ -154,12 +135,13 @@ def mongodb_query(input_query: str):
 
     # Invoke LLM
     mql = llm.invoke([message]).content
-    response = eval(f"collection.{mql}")
-    movie_list = []
-    if response:
-        for movie in response:
-            movie_list.append(movie)
-    return mql, movie_list
+    print(mql)
+    response = eval(f"collection.aggregate({mql})")
+    item_list = []
+    for item in response:
+        item_list.append(item)
+    print(item_list)
+    return item_list
 
 
 @mcp.tool()
